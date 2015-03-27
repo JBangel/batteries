7175f4ca2c6b95f256bd44a8c8ec75502b7a2fc1
Joshua Bangel <falornan@gmail.com>
2015-03-27 10:30:24 -0400

    Added test version of hosts config

--- install.sh
+++ install.sh
@@ -6,20 +6,27 @@ set -e
 install_homebrew() {
   SYSTEM=$(uname -s)
 
-  if [[ "$SYSTEM" == "Darwin" ]]; then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  else
-    sudo apt-get install -y build-essential curl git m4 ruby texinfo libbz2-dev libcurl4-openssl-dev libexpat-dev libncurses-dev zlib1g-dev
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
+#   if [[ "$SYSTEM" == "Darwin" ]]; then
+#     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#   else
+#     sudo apt-get install -y build-essential curl git m4 ruby texinfo libbz2-dev libcurl4-openssl-dev libexpat-dev libncurses-dev zlib1g-dev
+#     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
+# 
+#     export PATH="$HOME/.linuxbrew/bin:$PATH"
+#     export MANPATH="$HOME/.linuxbrew/share/man:$MANPATH"
+#     export INFOPATH="$HOME/.linuxbrew/share/info:$INFOPATH"
+#   fi
 
-    export PATH="$HOME/.linuxbrew/bin:$PATH"
-    export MANPATH="$HOME/.linuxbrew/share/man:$MANPATH"
-    export INFOPATH="$HOME/.linuxbrew/share/info:$INFOPATH"
+  if [[ "$SYSTEM" == "Darwin" ]]; then
+     echo "On OS X"
+   else
+     . $(curl https://raw.githubusercontent.com/JBangel/batteries/master/hosts/ubuntu | sh)
   fi
 }
 
+echo "$@"
+
 install_homebrew
 
-brew install rcm
-
-curl https://raw.githubusercontent.com/McPolemic/dotfiles/master/bin/install.sh | sh
+# brew install rcm
+# curl https://raw.githubusercontent.com/McPolemic/dotfiles/master/bin/install.sh | sh
